95427833dae70cf2749c05e5d16bad1c0b0ad0c2
datikken <tikken23@gmail.com>
2021-04-12 19:17:11 +0300

    ymaps added

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar'
+import { YMaps, Map } from 'react-yandex-maps';
 
 function App() {
     return (
         <div className="container">
             <Navbar/>
+            <YMaps>
+                <Map className="yndx_map" defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+            </YMaps>
         </div>
     );
 }
